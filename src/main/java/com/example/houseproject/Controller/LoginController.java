e798bf49a46d55f7bf13eebbc828d4e0e38e4a1c
zqf <1936534366@qq.com>
2022-02-24 17:56:03 +0800

    主页登录

--- src/main/java/com/example/houseproject/Controller/LoginController.java
+++ src/main/java/com/example/houseproject/Controller/LoginController.java
@@ -1,25 +1,16 @@
 package com.example.houseproject.Controller;
 
-import com.example.houseproject.Pojo.User;
 import com.example.houseproject.Service.UserService;
-import com.example.houseproject.Service.impl.UserServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
+@Controller
 public class LoginController {
     @Autowired
     private UserService userService;
-    @RequestMapping("/login")
-    public List<User> login(){
-        List list = new ArrayList();
-        list = userService.getAllUser();
-        System.out.println(list+"******");
-        return list;
+    @RequestMapping("/tologin")
+    public String tologin(){
+        return "login";
     }
 }
--- src/test/java/com/example/houseproject/HouseProjectApplicationTests.java
+++ src/test/java/com/example/houseproject/HouseProjectApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.houseproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class HouseProjectApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
